c78a8a8d883e8e64ac4c67b9e0f3f789e32d4674
Isaque D <isaqueduarte17@gmail.com>
2024-08-19 22:19:38 -0300

    refactor: fix typo in UserRepository Update method

--- cmd/api/repositories/user.repository.go
+++ cmd/api/repositories/user.repository.go
@@ -111,7 +111,7 @@ func (ur UserRepository) Delete(id int) error {
 	return nil
 }
 
-func (u UserRepository) Update(userPayload map[string]interface{}) error {
+func (ur UserRepository) Update(userPayload map[string]interface{}) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -137,7 +137,7 @@ func (u UserRepository) Update(userPayload map[string]interface{}) error {
 
 	values = append(values, id)
 
-	_, err := u.db.ExecContext(ctx, updateStatement+setStatement+whereStatement, values...)
+	_, err := ur.db.ExecContext(ctx, updateStatement+setStatement+whereStatement, values...)
 
 	if err != nil {
 		return fmt.Errorf("error while updating user: %v", err)
